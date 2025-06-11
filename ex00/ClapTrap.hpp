ed131c57545812150f7cd0614b3ef563f3f52032
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-11 18:21:01 +0100

    cpp03 - setup

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -13,6 +13,17 @@
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define BLUE    "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN    "\033[36m"
+# define RESET   "\033[0m"
+# define BOLD    "\033[1m"
+# define BLINK    "\033[5m" 
+# define REVERSE  "\033[7m"
+
 # include <iostream>
 # include <string>
 
@@ -24,6 +35,7 @@ class ClapTrap {
 		int			_hitPoints;
 		int			_energyPoints;
 		int			_attackDamage;
+
 	public:
 		ClapTrap(std::string &name);
 		~ClapTrap();
