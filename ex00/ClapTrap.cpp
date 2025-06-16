b6427b683b34838c97a1739c1bd0b72eaeb1d662
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-16 13:35:43 +0100

    cpp03 - setup

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -13,8 +13,9 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(std::string &name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
-	
+	std::cout << GREEN << "ClapTrap was created!!" << RESET << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
+	std::cout << RED << "ClapTrap was destroyed!" << RESET << std::endl;
 }
