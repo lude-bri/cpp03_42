8b57b72e3c02eb72353d8f249ed0a7ef916d57a4
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-16 22:08:08 +0100

    cpp03 - ex02 - melhorar main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,16 +6,18 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 21:57:43 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/06/16 22:04:41 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/06/16 22:06:29 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
+//melhorar main later
 int main(void)
 {
 	FragTrap hobbit("Samwise Gamgee");
 
 	hobbit.attack("Frodo");
+	hobbit.takeDamage(10);
 	hobbit.highFiveGuys();
 }
