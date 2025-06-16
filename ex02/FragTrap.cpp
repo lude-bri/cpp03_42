25348736560ad977441880d7ccd619d2f1e7c16c
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-16 22:02:34 +0100

    cpp03 - ex02 - freaking

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 21:36:48 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/06/16 21:55:29 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/06/16 22:02:07 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ FragTrap::FragTrap(const std::string &name) : ClapTrap(name) {
 	_hitPoints = 100;
 	_energyPoints = 100;
 	_attackDamage = 30;
-	std::cout << GREEN << "FragTrap " << YELLOW << _name << GREEN << " was created!"
+	std::cout << GREEN << "FragTrap " << YELLOW << _name << GREEN << " was FREAKING created!"
 		<< RESET << std::endl;
 }
 
 FragTrap::~FragTrap(){
-	std::cout << RED << "FragTrap " << YELLOW << _name << RED << " was destroyed!"
+	std::cout << RED << "FragTrap " << YELLOW << _name << RED << " was FREAKING destroyed!"
 		<< RESET << std::endl;
 }
 
