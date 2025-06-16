6a6a22d21334ee4b1f43e118cef634ba9719c001
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-16 21:34:04 +0100

    cpp03 - ex02 - setup

---
+++ ex02/ScavTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 21:33:32 by lude-bri          #+#    #+#             */
+/*   Updated: 2025/06/16 21:33:32 by lude-bri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+
+public:
+	ScavTrap(const std::string &name);
+	~ScavTrap();
+
+	void	attack(const std::string &target);
+	void	guardGate();
+};
+
+#endif
