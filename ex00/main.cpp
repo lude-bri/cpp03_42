ffc56edcfec0c12fc585d072a92b0efc05125918
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-17 18:08:44 +0100

    cpp03 - ex00 - new main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -15,12 +15,24 @@
 //melhorar a main
 int	main(void)
 {
+	std::cout << MAGENTA << "== TEST CLAPTRAP == " << RESET << std::endl;
+	
+	std::cout << YELLOW << " [Test 1. Building]" << RESET << std::endl;
 	ClapTrap	hobbit("Frodo");
-
 	hobbit.attack("Samwise Gamgee");
+
+	std::cout << YELLOW << " [Test 2. Damage Test]" << RESET << std::endl;
+	hobbit.takeDamage(5);
+	hobbit.beRepaired(3);
+
+	std::cout << YELLOW << " [Test 3. Draining Test]" << RESET << std::endl;
+	for (int i = 0; i < 9; i++)
+		hobbit.attack("Samwise");
+	
+	std::cout << YELLOW << " [Test 4. Impossible Repair and Attack]" << RESET << std::endl;
+	hobbit.takeDamage(20);
 	hobbit.beRepaired(10);
-	hobbit.takeDamage(10);
-	hobbit.takeDamage(10);
-	hobbit.takeDamage(10);
+	hobbit.attack("Gollum");
+
 	return 0;
 }
