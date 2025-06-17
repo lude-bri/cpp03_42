a8f52395481d00d5b68933e92a6a47e7e6b799b7
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-17 16:55:03 +0100

    cpp03 - FragTrap Orthodoxal Form

--- ex03/FragTrap.cpp
+++ ex03/FragTrap.cpp
@@ -12,6 +12,32 @@
 
 #include "FragTrap.hpp"
 
+//Default Constructor
+FragTrap::FragTrap() : ClapTrap("Default") {
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+	std::cout << BLUE << "FragTrap " << _name << " was FREAKING created" << RESET << std::endl;
+}
+
+//Copy Constructor
+FragTrap::FragTrap(const FragTrap &copy) : ClapTrap(copy) {
+	std::cout << BLUE << "FragTrap copy constructor called for " << _name << RESET << std::endl;
+}
+
+//Copy assignment Operator
+FragTrap &FragTrap::operator=(const FragTrap &copy) {
+	if (this != &copy) {
+		_name = copy._name;
+		_hitPoints = copy._hitPoints;
+		_energyPoints = copy._energyPoints;
+		_attackDamage = copy._attackDamage;
+		std::cout << BLUE << "FragTrap copy assignment constructor called for " << _name << RESET << std::endl;
+	}
+	return (*this);
+}
+
+//Constructor
 FragTrap::FragTrap(const std::string &name) : ClapTrap(name) {
 	_hitPoints = 100;
 	_energyPoints = 100;
@@ -20,11 +46,13 @@ FragTrap::FragTrap(const std::string &name) : ClapTrap(name) {
 		<< RESET << std::endl;
 }
 
+//Destructor
 FragTrap::~FragTrap(){
 	std::cout << BLUE << "FragTrap " << _name << " was FREAKING destroyed!"
 		<< RESET << std::endl;
 }
 
+//FragTrap highFive!!
 void	FragTrap::highFiveGuys(void) {
 	std::cout << BLUE << "FragTrap " << _name << " gives you a very nice high five!" << RESET << std::endl;
 }
